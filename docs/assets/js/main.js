4d5353157e19552ec8216f2e186e4b74ef687428
Dmitriy <1primo1@mail.ru>
2025-06-11 09:53:18 +0300

    finish

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -15351,6 +15351,112 @@ const stocksPrevSlider = new swiper_bundle__WEBPACK_IMPORTED_MODULE_0__["default
 
 });
 
+const pricesProfilePrevSlider = new swiper_bundle__WEBPACK_IMPORTED_MODULE_0__["default"]('.prices-prev__slider-profile', {
+    // Стрелки
+    navigation: {
+        nextEl: '.prices-prev__slider-next_profile',
+        prevEl: '.prices-prev__slider-prev_profile',
+    },
+
+    
+
+    /*Отступ у карточек*/
+    spaceBetween: 12,
+    /*Показывать по n карточек*/
+    slidesPerView: 1.5,
+    /* При достижении конца, перепрыгнуть в начало */
+    // rewind: true,
+    /*Увеличение при наведении курсора мыши */
+    // zoom: true,
+    /*Ленивая подгрузка */
+    // lazy: true,
+    /*Бесконечная прокрутка */
+    // loop: true,
+    /*Ориентация */
+    // direction: 'vertical',
+    /*Авто высота*/
+    // autoHeight: true,
+    /*иконка захвата при наведении на слайд*/
+    grabCursor: true,
+    /*Автоматическое перелистывание*/
+    // autoplay: {
+    //     delay: 5000,
+    //   },
+    /*Брек-поинты*/
+    breakpoints: {
+        1400: {
+            slidesPerView: 4.5,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 3.5,
+            spaceBetween: 15,
+        },
+        900: {
+            slidesPerView: 2.5,
+            spaceBetween: 15,
+        },
+        700: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        }
+    },
+
+});
+
+const pricesComparePrevSlider = new swiper_bundle__WEBPACK_IMPORTED_MODULE_0__["default"]('.prices-prev__slider_compare', {
+    // Стрелки
+    navigation: {
+        nextEl: '.prices-prev__slider-prev_compare',
+        prevEl: '.prices-prev__slider-next_compare',
+    },
+
+    
+
+    /*Отступ у карточек*/
+    spaceBetween: 12,
+    /*Показывать по n карточек*/
+    slidesPerView: 1.5,
+    /* При достижении конца, перепрыгнуть в начало */
+    // rewind: true,
+    /*Увеличение при наведении курсора мыши */
+    // zoom: true,
+    /*Ленивая подгрузка */
+    // lazy: true,
+    /*Бесконечная прокрутка */
+    // loop: true,
+    /*Ориентация */
+    // direction: 'vertical',
+    /*Авто высота*/
+    // autoHeight: true,
+    /*иконка захвата при наведении на слайд*/
+    grabCursor: true,
+    /*Автоматическое перелистывание*/
+    // autoplay: {
+    //     delay: 5000,
+    //   },
+    /*Брек-поинты*/
+    breakpoints: {
+        1400: {
+            slidesPerView: 4.5,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidesPerView: 3.5,
+            spaceBetween: 15,
+        },
+        900: {
+            slidesPerView: 2.5,
+            spaceBetween: 15,
+        },
+        700: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        }
+    },
+
+});
+
 const pricesPrevSlider = new swiper_bundle__WEBPACK_IMPORTED_MODULE_0__["default"]('.prices-prev__slider', {
     // Стрелки
     navigation: {
@@ -15582,7 +15688,7 @@ const portfolioPrevSlider = new swiper_bundle__WEBPACK_IMPORTED_MODULE_0__["defa
     /*Отступ у карточек*/
     spaceBetween: 12,
     /*Показывать по n карточек*/
-    slidesPerView: 1.2,
+    slidesPerView: 1.1,
     /* При достижении конца, перепрыгнуть в начало */
     // rewind: true,
     /*Увеличение при наведении курсора мыши */
@@ -29454,7 +29560,7 @@ __webpack_require__.r(__webpack_exports__);
 //------------Галлерея проектов---------------
 
 
-//------------Скрытие липкого плока в корзине---------------
+//------------Скрытие липкого блока в корзине---------------
 
 
 //------------Ввод одноразового кода из смс---------------
